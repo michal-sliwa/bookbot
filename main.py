333318d7fa418f29c350890234a0a8a3af6e5c45
michal-sliwa <michal_sliwa@icloud.com>
2025-03-02 22:36:54 +0100

    some fixes

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import sys
 from stats import get_num_words, get_char_count, sort_dictionaries
 
-book_path = "books/frankenstein.txt"
+
 
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
@@ -9,13 +10,17 @@ def get_book_text(path_to_file):
 
 
 def main():
-    text = get_book_text(book_path)
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    text = get_book_text(sys.argv[1])
     num_words = get_num_words(text)
     dict = get_char_count(text)
     sorted_list = sort_dictionaries(dict)
 
     print("============ BOOKBOT ============")
-    print(f"Analyzing book found at {book_path}...")
+    print(f"Analyzing book found at {sys.argv[1]}...")
     print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
     print("--------- Character Count -------")
